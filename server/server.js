5dd0f4d1f5dcd1b645dfb0d111026c5f29c97192
Guillermo <guillermo.sanchez.reyes9711@gmail.com>
2020-02-27 16:12:49 -0600

    RESTServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
 const bodyParser = require('body-parser');
 
@@ -10,42 +11,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+    (err, res) => {
 
-app.post('/usuario', function(req, res) {
+        if (err) throw err;
 
-    let body = req.body;
+        console.log('Database online');
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
     });
-});
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
-})
+});
