102455676b8b1b54523abda401cde0755708f01a
Guillermo <guillermo.sanchez.reyes9711@gmail.com>
2020-05-13 17:31:38 -0500

    Productos y Categorias

---
+++ server/routes/producto.js
@@ -0,0 +1,231 @@
+const express = require('express');
+
+const { verificaToken } = require('../middlewares/autenticacion');
+
+let app = express();
+let Producto = require('../models/producto');
+
+//====================
+// Obtener Productos
+//====================
+app.get('/producto', verificaToken, (req, res) => {
+    // trae todos los productos
+    // populate: usuario categoria
+    // paginado
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    Producto.find({ disponible: true })
+        .skip(desde)
+        .limit(5)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productos) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                productos
+            });
+        });
+});
+
+//===========================
+// Obtener un producto por ID
+//===========================
+app.get('/producto/:id', verificaToken, (req, res) => {
+    //populate: usuario categoria
+
+    let id = req.params.id;
+
+    Producto.findById(id)
+        .populate('usuario', 'nombre email')
+        .populate('categoria', 'descripcion')
+        .exec((err, productoDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            if (!productoDB) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'El ID no es válido'
+                    }
+                });
+            }
+
+            res.json({
+                ok: true,
+                productoDB
+            });
+        });
+});
+
+//===========================
+// Buscar productos
+//===========================
+app.get('/producto/buscar/:termino', verificaToken, (req, res) => {
+
+    let termino = req.params.termino;
+
+    let regex = new RegExp(termino, 'i');
+
+    Producto.find({ nombre: regex })
+        .populate('categoria', 'descripcion')
+        .exec((err, productos) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                productos
+            });
+        })
+})
+
+
+//===========================
+// Crear un nuevo producto
+//===========================
+app.post('/producto', verificaToken, (req, res) => {
+    // grabar el usuario
+    // grabar una categoria del listado
+    let body = req.body;
+    let producto = new Producto({
+        nombre: body.nombre,
+        precioUni: body.precioUni,
+        descripcion: body.descripcion,
+        categoria: body.categoria,
+        usuario: req.usuario._id
+    });
+
+    producto.save((err, productoDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            producto: productoDB
+        });
+    });
+});
+
+//===========================
+// Actualizar un producto
+//===========================
+app.put('/producto/:id', verificaToken, (req, res) => {
+    // grabar el usuario
+    // grabar una categoria del listado
+    let id = req.params.id;
+    let body = req.body;
+
+    let descProducto = {
+        descripcion: body.descripcion
+    }
+
+    Producto.findByIdAndUpdate(id, descProducto, { new: true, runValidators: true }, (err, productoDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        productoDB.nombre = body.nombre;
+        productoDB.precioUni = body.precioUni;
+        productoDB.categoria = body.categoria;
+        productoDB.disponible = body.disponible;
+        productoDB.descripcion = body.descripcion;
+
+        productoDB.save((err, productoGuardado) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                producto: productoDB
+            });
+        })
+    });
+});
+
+//===========================
+// Borrar un nuevo producto
+//===========================
+app.delete('/producto/:id', verificaToken, (req, res) => {
+    // grabar el usuario
+    // grabar una categoria del listado
+    let id = req.params.id;
+
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!productoDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'El ID no existe'
+                }
+            });
+        }
+
+        productoDB.disponible = false;
+        productoDB.save((err, productoBorrado) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                producto: productoBorrado,
+                message: 'Producto Borrado'
+            });
+        })
+    });
+});
+
+module.exports = app;
